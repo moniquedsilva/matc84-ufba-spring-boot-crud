adaafd9fdda9bb6e8a571f58d48a2a56b85e896c
Monique Silva <moniquesntsilva@gmail.com>
2023-07-03 22:08:50 -0300

    test

--- src/main/java/br/matc84/spring/exercicio/domain/ports/InvoiceServicePort.java
+++ src/main/java/br/matc84/spring/exercicio/domain/ports/InvoiceServicePort.java
@@ -5,10 +5,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import br.matc84.spring.exercicio.domain.models.InvoiceModel;
-import br.matc84.spring.exercicio.api.dtos.InvoiceDTO; 
 
 public interface InvoiceServicePort {
     public List<InvoiceModel> getAll();
     public InvoiceModel findById(String uuid);
-    public InvoiceModel create(InvoiceDTO invoiceDTO);
+    public InvoiceModel create(BigDecimal totalValue, LocalDate dueDate);
 }
